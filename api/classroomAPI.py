1c91ee3cd2b9fd8ab007108f2a5b722538ca7354
Fábio Oliveira <cbarrossilva123@gmail.com>
2023-09-03 13:24:31 -0300

    Primeiro commit

---
+++ api/classroomAPI.py
@@ -0,0 +1,23 @@
+from .google_auth import getCreds
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def printCourses():
+    try:
+        creds = getCreds()
+        service = build('classroom', 'v1', credentials=creds)
+
+        # Call the Classroom API
+        results = service.courses().list(pageSize=10).execute()
+        courses = results.get('courses', [])
+
+        if not courses:
+            print('No courses found.')
+            return
+        # Prints the names of the first 10 courses.
+        print('Courses:')
+        for course in courses:
+            print(course['name'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
---
+++ main.py
@@ -0,0 +1,7 @@
+from __future__ import print_function
+from api import printCourses
+
+if __name__ == '__main__':
+    printCourses()
+
+
